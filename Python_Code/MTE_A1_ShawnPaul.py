badcfb4a74cc4981cf797e0165f35dd506ed7f63
shawnpaul123 <31085457+shawnpaul123@users.noreply.github.com>
2021-03-22 23:19:02 -0400

    started adding funcs/vars

--- Python_Code/MTE_A1_ShawnPaul.py
+++ Python_Code/MTE_A1_ShawnPaul.py
@@ -1,7 +1,47 @@
 import numpy as np  
-
+from var_store import var_store
+import math as m
 
 class EKF_system:
 
-	def __init__(self):
-		self.var = ""
+	def __init__(var_store):
+		super.__init__()
+
+	def Fk(self):
+		pass
+
+	def Pk(self,Pk_m1):
+		return np.matmul(np.matmul(self.Fk,Pk_m1),np.transpose(Fk)) + self.Q
+
+	def del_dot_m1(self,del_k_m1,del_k_m2):
+		return (del_k_m1 - del_k_m2)/self.delta_t
+
+	def Bk_m1(self,del_k_m1):
+		return m.atan((self.Lr/self.L)*m.tan(del_k_m1))
+
+	def Ff(self):
+		return self.Tk_m1/self.r 
+
+	def fi(self,vk_m1,del_k_m1,beta_k_m1):
+		return self.mu_i*self.m*self.g*np.sign(vk_m1*cos(del_k_m1-beta_k_m1))
+
+	def pred_step(self):
+		pass
+######################################################################################
+
+	def P_corr(self):
+		pass
+
+	def Kk_corr(self):
+		pass
+
+	def Hk(self):
+		pass
+
+	def Xk_pred_corr(self):
+		pass
+
+
+	def Yk(self):
+		pass
+
--- Python_Code/var_store.py
+++ Python_Code/var_store.py
@@ -5,9 +5,27 @@ class var_store:
 
 	def __init__(self):
 
-		#for ekf -> could be used elsewhere
+		#global state vars
 
 		self.state_rows = 3
 		self.state_cols = 1
+
+		#ekf variables
+		#T: Front wheel drive torque [N-m]		
+		self.Tk_m1 = ""
+		#r: Wheel radius [m]
+		self.r = ""
+		self.mu_i = ""
+		self.m = ""
+		self.g = 9.81
+		self.Lr = 1
+		self.L = 1
+		self.delta_t = ""
+		self.Q = ""
+		
+
+
+
+
 
 
