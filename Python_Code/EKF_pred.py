6532374464c9d577637a5881d9f284b3cbf3b77d
shawnpaul123 <31085457+shawnpaul123@users.noreply.github.com>
2021-03-29 12:21:35 -0400

    made changes to variables/funcs

--- Python_Code/EKF_pred.py
+++ Python_Code/EKF_pred.py
@@ -7,11 +7,13 @@ class EKF_pred(var_store):
 	def __init__(self):
 		super().__init__()
 
-
+	#Throttle setting, 0<T<1
 	def Tk(self):
-		#subject to change if time dependant	
-		return self.Tk_m1
+		#subject to change if time dependat
+		Tk = self.Tk_m1	
+		return Tk_m1
 
+	#State covariance
 	def Pk(self,Pk_m1):
 		Fk = self.Fk()
 		return np.matmul(np.matmul(Fk,Pk_m1),np.transpose(Fk)) + self.Q
@@ -20,25 +22,36 @@ class EKF_pred(var_store):
 	def del_dot_m1(self,del_k_m1,del_k_m2):
 		return (del_k_m1 - del_k_m2)/self.dt
 
+	def ad(self,v):
+		return self.b()*v
+
+	def af(self,T,v):
+		return self.c1(T)*v + self.c2(T)
+
+	#Specific power model parameter 1 [m/s2]
+	def c1(self,T):
+		return -0.8304*(T**2)+1.6639*T
+
+	#Specific power model parameter 1 [m/s2]
+	def c2(self,T):
+		return -0.0323*T**2+0.4115*T
+
+	#direction of motion for COG
 	def Bk_m1(self,del_k_m1):
 		return m.atan((self.L_r/self.L)*m.tan(del_k_m1))
 
-	def F_f(self):
-		return self.Tk()/self.r 
 
-	def fi(self,vk_m1,del_k_m1,beta_k_m1):
-		return self.mu_i*self.m*self.g*np.sign(vk_m1*m.cos(del_k_m1-beta_k_m1))
-
+	#jacobian of f
 	def Fk(self):
-		#jacobian of f
-		#3x3 matrix
-		#insert jacobian from pde file
-		#returns identity for now
 		return np.random.rand(3,3)
 
+	#friction coefficient
+	def b(self,s=1):
+		return 0.365056*(s**(-1))
 
 
-	
+
+	#State prediction
 	def Xk_pred(self,Xk_pred_corr_k_m1,Uk_m1):
 		vk_m1 = Xk_pred_corr_k_m1[0,:]
 		thetha_k_m1 = Xk_pred_corr_k_m1[1,:]
@@ -46,10 +59,25 @@ class EKF_pred(var_store):
 		T_k_m1 = Uk_m1[0,:]
 		del_k_m1 = Uk_m1[1,:]
 		del_k_m2 = del_k_m1
-		vk = vk_m1 + (((self.F_f() - self.f_f)*m.cos(del_k_m1 - self.Bk_m1(del_k_m1)) - self.f_r*m.cos(self.Bk_m1(del_k_m1)) -0.5*self.A*self.C_d*vk_m1*vk_m1)*self.dt)/self.m
+		vk = vk_m1 + (self.af(T_k_m1,vk_m1) - self.ad(vk_m1))*self.dt
 		thetha_k = thetha_k_m1 + (self.L*m.tan(del_k_m1))/(self.L**2 + (self.L_r**2)*(m.tan(del_k_m1)**2))*vk_m1*self.dt
-		w_k = w_k_m1 + ((self.L*(self.L**2 - (self.L_r**2)*(m.tan(del_k_m1)**2)))/((((self.L_r**2)*(m.tan(del_k_m1)**2))**2)*(m.cos(del_k_m1)**2)))*vk_m1*self.del_dot_m1(del_k_m1,del_k_m2)*self.dt
+		w_k = w_k_m1 + ((self.L*(self.L**2 - (self.L_r**2)*(m.tan(del_k_m1)**2)))/((((self.L_r**2)*(m.tan(del_k_m1)**2))**2)*(m.cos(del_k_m1)**2)))*vk_m1*self.del_dot_m1(del_k_m1,self.del_k_m2)*self.dt
+		self.del_k_m2 = del_k_m1
 		#insert assertion on shape type
-		return np.array([vk,thetha_k,w_k])
+		ret_val = np.array([vk,thetha_k,w_k])
+		assert ret_val.shape == (3,1)
+		return ret_val
 
 
+
+	'''
+	def F_f(self):
+		return self.Tk()/self.r 
+
+	def fi(self,vk_m1,del_k_m1,beta_k_m1):
+		return self.mu_i*self.m*self.g*np.sign(vk_m1*m.cos(del_k_m1-beta_k_m1))
+	'''
+
+
+if __name__ == '__main__':
+	E = EKF_pred()
--- Python_Code/var_store.py
+++ Python_Code/var_store.py
@@ -16,34 +16,26 @@ class var_store:
 		self.Xk = np.random.rand(3,1)
 ###########################################################################################
 		#loaded variables -> data being loaded in from csv(probably pandas)
+		self.case_name = ""
 		self.csv = ""
 ###########################################################################################
-		#control and state variables -> designated as methods or vars otherwise below
-		#rolling friction force
-		self.f_f = 5	
-		self.f_r = 5
+		#control and state vars
+		#mostly methods
 ###########################################################################################
 		#Model constants
 		#!Values subject to change
 		#r: Wheel radius [m]
-		self.r = 0.5
-		#Axle coefficient of friction for wheel 𝑖; 𝜇𝑓 for front wheel, 𝜇𝑟 for rear wheel
-		self.mu_i = 0.5
-		#Vehicle mass [kg]
-		self.m = 10
-		#gravitational force
-		self.g = 9.81
 		#Distance between rear wheel axis and COG [m]
 		self.L_r = 1
 		#Length between rear and front wheel axes [m]
 		self.L = 1
-		#Frontal vehicle area [m2]
-		self.A = 5
 		#difference in time
 		self.dt = 0.05
-		#Coefficient of drag
-		self.C_d = 0.81
+		
 ###########################################################################################
+	#get data from csv
+	def get_data():
+		return None
 
 
 '''
