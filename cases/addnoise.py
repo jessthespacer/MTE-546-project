d698deae48f21726cf0dc844c4ad33fc9f35e70c
kapz28 <KaSatkunanathan@gmail.com>
2021-04-03 23:59:11 -0400

    Add files via upload

--- cases/addnoise.py
+++ cases/addnoise.py
@@ -1,10 +1,10 @@
 import csv
 import numpy as np
 
-with open('MVPcase.csv') as csv_file:
+with open('MVPcasefinal.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
-    with open('MVPwithnoise.csv', mode='w', newline='') as write_file:
+    with open('MVPcasefinalwithnoise.csv', mode='w', newline='') as write_file:
         file_writer = csv.writer(write_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for row in csv_reader:
             if line_count == 0:
@@ -12,8 +12,9 @@ with open('MVPcase.csv') as csv_file:
                 file_writer.writerow(row)
             else:
                 row = [float(i) for i in row]
-                row[3] += np.random.normal(0, 1)
-                row[4] += np.random.normal(0, 1)
+                row[3] += np.random.normal(0, .5)
+                row[4] += np.random.normal(0, .1)
+                row[6] += np.random.normal(0, .1)
 
                 file_writer.writerow(row)
 
